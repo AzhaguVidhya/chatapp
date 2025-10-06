5b1e8f4de6f9cdba75029298c092aa6cc89982db
AzhaguVidhya <azhaguvidhya049@gmail.com>
2025-10-06 12:42:50 +0530

    Update index.js

--- socket/index.js
+++ socket/index.js
@@ -1,48 +1,49 @@
 const { Server } = require("socket.io");
 
-const cors = require("cors");
+const io = new Server({
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
 
-const io = new Server({cors:" http://localhost:5173"});
-
-let onlineUsers=[];
+let onlineUsers = [];
 
 io.on("connection", (socket) => {
-  console.log("new connection",socket.id);
+  console.log("New connection:", socket.id);
 
-  //listen to a connection
-  socket.on("addNewUser",(userId)=>{
-    !onlineUsers.some((user)=>user.userId === userId) &&
-    onlineUsers.push({
-      userId,
-      socketId : socket.id,
-          });
-
-          console.log("onlineUsers",onlineUsers);
-          io.emit("getOnlineUsers",onlineUsers);
+  // Listen for new user
+  socket.on("addNewUser", (userId) => {
+    if (!onlineUsers.some((user) => user.userId === userId)) {
+      onlineUsers.push({
+        userId,
+        socketId: socket.id,
+      });
+    }
+    console.log("Online users:", onlineUsers);
+    io.emit("getOnlineUsers", onlineUsers);
   });
 
-  //add message
-  socket.on("sendMessage",(message)=>{
-    const user=onlineUsers.find((user) => user.userId === message.recipientId);
-
-    if(user){
-      io.to(user.socketId).emit("getMessage",message);
-      io.to(user.socketId).emit("getNotification",{
-        senderId:message.senderId,
-        isRead:false,
-        date:new Date(),
+  // Listen for sending message
+  socket.on("sendMessage", (message) => {
+    const user = onlineUsers.find((user) => user.userId === message.recipientId);
+    if (user) {
+      io.to(user.socketId).emit("getMessage", message);
+      io.to(user.socketId).emit("getNotification", {
+        senderId: message.senderId,
+        isRead: false,
+        date: new Date(),
       });
     }
   });
-  
 
-  socket.on("disconnect",()=> {
-    onlineUsers=onlineUsers.filter(user => user.socketId !== socket.id)
-
-              io.emit("getOnlineUsers",onlineUsers);
-
-  })
+  // On disconnect
+  socket.on("disconnect", () => {
+    onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+    io.emit("getOnlineUsers", onlineUsers);
+  });
 });
 
-
+// Start Socket.IO server on port 3000
 io.listen(3000);
+console.log("Socket.IO server listening on port 3000");
